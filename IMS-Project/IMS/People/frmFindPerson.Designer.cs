5c66c1c93969e53215505021bf5a6ea47a11cc6c
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-07-05 16:05:54 +0300

    Find Person Form

---
+++ IMS-Project/IMS/People/frmFindPerson.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace IMS.People
+{
+    partial class frmFindPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlPersonCardWithFilter1 = new IMS.People.Controls.ctrlPersonCardWithFilter();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCardWithFilter1
+            // 
+            this.ctrlPersonCardWithFilter1.Location = new System.Drawing.Point(12, 12);
+            this.ctrlPersonCardWithFilter1.Name = "ctrlPersonCardWithFilter1";
+            this.ctrlPersonCardWithFilter1.SearchEnabled = true;
+            this.ctrlPersonCardWithFilter1.ShowAddPerson = true;
+            this.ctrlPersonCardWithFilter1.Size = new System.Drawing.Size(862, 395);
+            this.ctrlPersonCardWithFilter1.TabIndex = 0;
+            // 
+            // frmFindPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 406);
+            this.Controls.Add(this.ctrlPersonCardWithFilter1);
+            this.Name = "frmFindPerson";
+            this.Text = "frmFindPerson";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.ctrlPersonCardWithFilter ctrlPersonCardWithFilter1;
+    }
+}
