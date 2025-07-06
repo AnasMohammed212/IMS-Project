4a57dca98b518b7266d49baeeb06db508ad3e919
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-07-06 23:07:11 +0300

    Change Password Form

--- IMS-Project/IMS/Users/frmChangePassword.Designer.cs
+++ IMS-Project/IMS/Users/frmChangePassword.Designer.cs
@@ -28,19 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCurrentPassword = new System.Windows.Forms.TextBox();
+            this.ctrlUserCard1 = new IMS.Users.ctrlUserCard();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(821, 581);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(131, 44);
+            this.btnSave.TabIndex = 26;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(684, 581);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(131, 44);
+            this.btnClose.TabIndex = 25;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 548);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 25);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "New Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 600);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Confirm Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 498);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Current Password";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::IMS.Properties.Resources.Password;
+            this.pictureBox3.Location = new System.Drawing.Point(199, 597);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 21;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(234, 599);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(165, 28);
+            this.txtConfirmPassword.TabIndex = 20;
+            this.txtConfirmPassword.Validating += new System.ComponentModel.CancelEventHandler(this.txtConfirmPassword_Validating);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::IMS.Properties.Resources.Password;
+            this.pictureBox2.Location = new System.Drawing.Point(199, 545);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewPassword.Location = new System.Drawing.Point(234, 547);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.Size = new System.Drawing.Size(165, 28);
+            this.txtNewPassword.TabIndex = 18;
+            this.txtNewPassword.Validating += new System.ComponentModel.CancelEventHandler(this.txtNewPassword_Validating);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IMS.Properties.Resources.Password;
+            this.pictureBox1.Location = new System.Drawing.Point(199, 495);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtCurrentPassword
+            // 
+            this.txtCurrentPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCurrentPassword.Location = new System.Drawing.Point(234, 497);
+            this.txtCurrentPassword.Name = "txtCurrentPassword";
+            this.txtCurrentPassword.Size = new System.Drawing.Size(165, 28);
+            this.txtCurrentPassword.TabIndex = 16;
+            this.txtCurrentPassword.Validating += new System.ComponentModel.CancelEventHandler(this.txtCurrentPassword_Validating);
+            // 
+            // ctrlUserCard1
+            // 
+            this.ctrlUserCard1.Location = new System.Drawing.Point(12, 8);
+            this.ctrlUserCard1.Name = "ctrlUserCard1";
+            this.ctrlUserCard1.Size = new System.Drawing.Size(887, 457);
+            this.ctrlUserCard1.TabIndex = 27;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // frmChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1201, 504);
+            this.ClientSize = new System.Drawing.Size(974, 650);
+            this.Controls.Add(this.ctrlUserCard1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtCurrentPassword);
             this.Name = "frmChangePassword";
             this.Text = "frmChangePassword";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtCurrentPassword;
+        private ctrlUserCard ctrlUserCard1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- IMS-Project/IMS/Users/frmChangePassword.cs
+++ IMS-Project/IMS/Users/frmChangePassword.cs
@@ -7,14 +7,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IMS_Business;
 
 namespace IMS.Users
 {
     public partial class frmChangePassword : Form
     {
-        public frmChangePassword()
+        private int _UserID;
+        private clsUser _User;
+        public frmChangePassword(int UserID)
         {
             InitializeComponent();
+            _UserID = UserID;
+        }
+        private void _ResetDefaultValues()
+        {
+            txtCurrentPassword.Text = "";
+            txtNewPassword.Text = "";
+            txtConfirmPassword.Text = "";
+            txtCurrentPassword.Focus();
+        }
+        private void frmChangePassword_Load(object sender, EventArgs e)
+        {
+            _User = clsUser.FindByUserID(_UserID);
+            if (_User == null)
+            {
+                MessageBox.Show("Could not Find User with id = " + _UserID,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+            ctrlUserCard1.LoadUserInfo(_UserID);
+        }
+
+
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro",
+                    "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _User.Password = txtNewPassword.Text;
+            if (await _User.Save())
+            {
+                MessageBox.Show("Password Changed Successfully.",
+                   "Saved.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                _ResetDefaultValues();
+            }
+            else
+                MessageBox.Show("An Erro Occured, Password did not change.",
+                  "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void txtCurrentPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCurrentPassword.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtCurrentPassword, "Current Password cannot be Empty");
+                return;
+            }
+            else
+                errorProvider1.SetError(txtCurrentPassword, null);
+            if (_User.Password != txtCurrentPassword.Text.Trim())
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtCurrentPassword, "Current password is wrong!");
+                return;
+            }
+            else
+                errorProvider1.SetError(txtCurrentPassword, null);
+        }
+
+        private void txtNewPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNewPassword.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtNewPassword, "New Password cannot be Empty");
+                return;
+            }
+            else
+                errorProvider1.SetError(txtNewPassword, null);
+        }
+
+        private void txtConfirmPassword_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtConfirmPassword.Text.Trim() != txtNewPassword.Text.Trim())
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtConfirmPassword, "Password Confirmation does not match New Password!");
+                return;
+            }
+            else
+                errorProvider1.SetError(txtNewPassword, null);
         }
     }
 }
