f67c349ec9202cb495d0f114d650e326f66974ba
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-07-13 22:11:46 +0300

    Product Card Control

---
+++ IMS-Project/IMS/Products/ctrlProductCard.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace IMS.Products
+{
+    partial class ctrlProductCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.lblUnit = new System.Windows.Forms.Label();
+            this.lblSupplier = new System.Windows.Forms.Label();
+            this.lblPurchasePrice = new System.Windows.Forms.Label();
+            this.lblSalePrice = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.aaa = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.pbGendor = new System.Windows.Forms.PictureBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label26 = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGendor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(418, 191);
+            this.lblDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(68, 25);
+            this.lblDescription.TabIndex = 161;
+            this.lblDescription.Text = "[????]";
+            // 
+            // lblUnit
+            // 
+            this.lblUnit.AutoSize = true;
+            this.lblUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUnit.Location = new System.Drawing.Point(492, 104);
+            this.lblUnit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUnit.Name = "lblUnit";
+            this.lblUnit.Size = new System.Drawing.Size(68, 25);
+            this.lblUnit.TabIndex = 160;
+            this.lblUnit.Text = "[????]";
+            // 
+            // lblSupplier
+            // 
+            this.lblSupplier.AutoSize = true;
+            this.lblSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSupplier.Location = new System.Drawing.Point(492, 67);
+            this.lblSupplier.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSupplier.Name = "lblSupplier";
+            this.lblSupplier.Size = new System.Drawing.Size(68, 25);
+            this.lblSupplier.TabIndex = 159;
+            this.lblSupplier.Text = "[????]";
+            // 
+            // lblPurchasePrice
+            // 
+            this.lblPurchasePrice.AutoSize = true;
+            this.lblPurchasePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPurchasePrice.Location = new System.Drawing.Point(181, 109);
+            this.lblPurchasePrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPurchasePrice.Name = "lblPurchasePrice";
+            this.lblPurchasePrice.Size = new System.Drawing.Size(68, 25);
+            this.lblPurchasePrice.TabIndex = 158;
+            this.lblPurchasePrice.Text = "[????]";
+            // 
+            // lblSalePrice
+            // 
+            this.lblSalePrice.AutoSize = true;
+            this.lblSalePrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalePrice.Location = new System.Drawing.Point(147, 146);
+            this.lblSalePrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSalePrice.Name = "lblSalePrice";
+            this.lblSalePrice.Size = new System.Drawing.Size(68, 25);
+            this.lblSalePrice.TabIndex = 156;
+            this.lblSalePrice.Text = "[????]";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblProductName.Location = new System.Drawing.Point(181, 67);
+            this.lblProductName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(74, 25);
+            this.lblProductName.TabIndex = 155;
+            this.lblProductName.Text = "[????]";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(13, 34);
+            this.label22.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(126, 25);
+            this.label22.TabIndex = 139;
+            this.label22.Text = "Product ID :";
+            // 
+            // aaa
+            // 
+            this.aaa.AutoSize = true;
+            this.aaa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aaa.Location = new System.Drawing.Point(12, 109);
+            this.aaa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.aaa.Name = "aaa";
+            this.aaa.Size = new System.Drawing.Size(171, 25);
+            this.aaa.TabIndex = 153;
+            this.aaa.Text = "Purchase Price :";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductID.Location = new System.Drawing.Point(141, 34);
+            this.lblProductID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(68, 25);
+            this.lblProductID.TabIndex = 151;
+            this.lblProductID.Text = "[????]";
+            // 
+            // pbGendor
+            // 
+            this.pbGendor.Image = global::IMS.Properties.Resources.Person;
+            this.pbGendor.Location = new System.Drawing.Point(122, 109);
+            this.pbGendor.Name = "pbGendor";
+            this.pbGendor.Size = new System.Drawing.Size(31, 26);
+            this.pbGendor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbGendor.TabIndex = 154;
+            this.pbGendor.TabStop = false;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(372, 146);
+            this.label19.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(127, 25);
+            this.label19.TabIndex = 145;
+            this.label19.Text = "Description:";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(12, 185);
+            this.label20.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(113, 25);
+            this.label20.TabIndex = 144;
+            this.label20.Text = "Category :";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label21.Location = new System.Drawing.Point(20, 146);
+            this.label21.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(118, 25);
+            this.label21.TabIndex = 143;
+            this.label21.Text = "Sale Price:";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(385, 104);
+            this.label23.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(57, 25);
+            this.label23.TabIndex = 142;
+            this.label23.Text = "Unit:";
+            // 
+            // label24
+            // 
+            this.label24.AutoSize = true;
+            this.label24.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label24.Location = new System.Drawing.Point(372, 67);
+            this.label24.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(99, 25);
+            this.label24.TabIndex = 141;
+            this.label24.Text = "Supplier:";
+            // 
+            // label26
+            // 
+            this.label26.AutoSize = true;
+            this.label26.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label26.Location = new System.Drawing.Point(13, 67);
+            this.label26.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label26.Name = "label26";
+            this.label26.Size = new System.Drawing.Size(155, 25);
+            this.label26.TabIndex = 140;
+            this.label26.Text = "Product Name:";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategory.Location = new System.Drawing.Point(134, 185);
+            this.lblCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(68, 25);
+            this.lblCategory.TabIndex = 157;
+            this.lblCategory.Text = "[????]";
+            // 
+            // ctrlProductCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblUnit);
+            this.Controls.Add(this.lblSupplier);
+            this.Controls.Add(this.lblPurchasePrice);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblSalePrice);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.aaa);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.pbGendor);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label26);
+            this.Name = "ctrlProductCard";
+            this.Size = new System.Drawing.Size(704, 283);
+            this.Load += new System.EventHandler(this.ctrlProductCard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGendor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Label lblUnit;
+        private System.Windows.Forms.Label lblSupplier;
+        private System.Windows.Forms.Label lblPurchasePrice;
+        private System.Windows.Forms.Label lblSalePrice;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label aaa;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.PictureBox pbGendor;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label26;
+        private System.Windows.Forms.Label lblCategory;
+    }
+}
---
+++ IMS-Project/IMS/Products/ctrlProductCard.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IMS.Properties;
+using IMS_Business;
+
+namespace IMS.Products
+{
+    public partial class ctrlProductCard : UserControl
+    {
+        private clsProduct _Product;
+        private int _ProductID = -1;
+        public int ProductID { get { return _ProductID; } }
+        public clsProduct SelectedProductInfo { get { return _Product; } }
+        public ctrlProductCard()
+        {
+            InitializeComponent();
+        }
+        public void ResetProductInfo()
+        {
+            _ProductID = -1;
+            lblProductID.Text = "[????]";
+            lblDescription.Text = "[????]";
+            lblProductName.Text = "[????]";
+            lblPurchasePrice.Image = Resources.Male;
+            lblSalePrice.Text = "[????]";
+            lblCategory.Text = "[????]";
+            lblSupplier.Text = "[????]";
+            lblUnit.Text = "[????]";
+        }
+        private void _FillProductInfo()
+        {
+            
+            _ProductID = _Product.ProductID;
+            lblProductID.Text = _Product.ProductID.ToString();
+            lblDescription.Text = _Product.Description;
+            lblProductName.Text = _Product.ProductName;
+            lblPurchasePrice.Text = _Product.PurchasePrice.ToString();
+            lblSalePrice.Text = _Product.SalePrice.ToString();
+            lblCategory.Text = _Product.CategoryInfo.CategoryName;
+            lblSupplier.Text = _Product.SupplierInfo.SupplierName;
+            lblUnit.Text = _Product.UnitInfo.UnitName;
+            
+        }
+        public void LoadProductInfo(int ProductID)
+        {
+            _Product = clsProduct.Find(ProductID);
+            if (_Product == null)
+            {
+                ResetProductInfo();
+                MessageBox.Show("No Product With ProductID = " + ProductID.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            _FillProductInfo();
+        }
+        private void ctrlProductCard_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
