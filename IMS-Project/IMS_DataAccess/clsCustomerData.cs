1e5fcc36ba528b790f16c07112fbfcf681a8e7d1
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-07-18 20:43:23 +0300

    Customer Data Layer

---
+++ IMS-Project/IMS_DataAccess/clsCustomerData.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS_DataAccess
+{
+    public class clsCustomerData
+    {
+        public static bool GetCustomerInfoByID(int CustomerID, ref string CustomerName, ref int ContactPersonID)
+        {
+            bool isFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SP_GetCustomerInfoByID", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            CustomerName = reader["CustomerName"] as string ?? "";
+                            ContactPersonID = reader["CustomerContactPerson"] != DBNull.Value ? (int)reader["CustomerContactPerson"] : -1;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isFound;
+        }
+
+        public static bool GetCustomerInfoByName(ref int CustomerID, string CustomerName, ref int ContactPersonID)
+        {
+            bool isFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SP_GetCustomerByName", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerName", CustomerName);
+                        SqlDataReader reader = command.ExecuteReader();
+                        if (reader.Read())
+                        {
+                            isFound = true;
+                            CustomerID = (int)reader["CustomerID"];
+                            ContactPersonID = reader["CustomerContactPerson"] != DBNull.Value ? (int)reader["CustomerContactPerson"] : -1;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isFound;
+        }
+
+        public static async Task<DataTable> GetAllCustomers()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_GetAllCustomers", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        SqlDataReader reader = await command.ExecuteReaderAsync();
+                        if (reader.HasRows)
+                            dt.Load(reader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return dt;
+        }
+
+        public static async Task<bool> IsCustomerExist(int CustomerID)
+        {
+            bool isFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_IsCustomerExist", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        SqlParameter returnParameter = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.ReturnValue
+                        };
+                        command.Parameters.Add(returnParameter);
+                        await command.ExecuteNonQueryAsync();
+                        isFound = (int)returnParameter.Value == 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isFound;
+        }
+
+        public static async Task<bool> IsCustomerExist(string CustomerName)
+        {
+            bool isFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_IsCustomerExistByName", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerName", CustomerName);
+                        SqlParameter returnParameter = new SqlParameter("@ReturnVal", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.ReturnValue
+                        };
+                        command.Parameters.Add(returnParameter);
+                        await command.ExecuteNonQueryAsync();
+                        isFound = (int)returnParameter.Value == 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return isFound;
+        }
+
+        public static async Task<int> AddNewCustomer(string CustomerName, int ContactPersonID)
+        {
+            int NewCustomerID = -1;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_AddNewCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerName", CustomerName);
+                        command.Parameters.AddWithValue("@CustomerContactPerson", ContactPersonID == -1 ? (object)DBNull.Value : ContactPersonID);
+
+                        SqlParameter outputIdParam = new SqlParameter("@NewCustomerID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        await command.ExecuteNonQueryAsync();
+                        NewCustomerID = (int)command.Parameters["@NewCustomerID"].Value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return NewCustomerID;
+        }
+
+        public static async Task<bool> UpdateCustomer(int CustomerID, string CustomerName, int ContactPersonID)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_UpdateCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        command.Parameters.AddWithValue("@CustomerName", CustomerName);
+                        command.Parameters.AddWithValue("@CustomerContactPerson", ContactPersonID == -1 ? (object)DBNull.Value : ContactPersonID);
+                        rowsAffected = await command.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return (rowsAffected > 0);
+        }
+
+        public static async Task<bool> DeleteCustomer(int CustomerID)
+        {
+            int rowsAffected = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    await connection.OpenAsync();
+                    using (SqlCommand command = new SqlCommand("SP_DeleteCustomer", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@CustomerID", CustomerID);
+                        rowsAffected = await command.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return (rowsAffected > 0);
+        }
+    }
+}
