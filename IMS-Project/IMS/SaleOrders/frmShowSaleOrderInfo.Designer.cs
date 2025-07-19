34ebddab0c2b6c3d31e8cc36970a15285efa77d4
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2025-07-19 19:34:14 +0300

    Show Sale Order Info Form

---
+++ IMS-Project/IMS/SaleOrders/frmShowSaleOrderInfo.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace IMS.SaleOrders
+{
+    partial class frmShowSaleOrderInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlSaleOrderInfo1 = new IMS.SaleOrders.ctrlSaleOrderInfo();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctrlSaleOrderInfo1
+            // 
+            this.ctrlSaleOrderInfo1.Location = new System.Drawing.Point(12, 21);
+            this.ctrlSaleOrderInfo1.Name = "ctrlSaleOrderInfo1";
+            this.ctrlSaleOrderInfo1.Size = new System.Drawing.Size(555, 258);
+            this.ctrlSaleOrderInfo1.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(477, 304);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(90, 38);
+            this.btnClose.TabIndex = 107;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmShowSaleOrderInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 354);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.ctrlSaleOrderInfo1);
+            this.Name = "frmShowSaleOrderInfo";
+            this.Text = "frmSaleOrderInfo";
+            this.Load += new System.EventHandler(this.frmShowSaleOrderInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlSaleOrderInfo ctrlSaleOrderInfo1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ IMS-Project/IMS/SaleOrders/frmShowSaleOrderInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMS.SaleOrders
+{
+    public partial class frmShowSaleOrderInfo : Form
+    {
+        private int _SaleOrderID;
+        public frmShowSaleOrderInfo(int SaleOrderID)
+        {
+            InitializeComponent();
+            _SaleOrderID = SaleOrderID;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmShowSaleOrderInfo_Load(object sender, EventArgs e)
+        {
+            ctrlSaleOrderInfo1.LoadSaleOrderInfo(_SaleOrderID);
+        }
+    }
+}
